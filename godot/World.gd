4dbe17ced51c3c7fd83e653fdd5e9db20d0a0bf1
Marco <mrcgll98@gmail.com>
2019-10-06 19:14:40 +0200

    Legal moves and its checks WIP

--- godot/World.gd
+++ godot/World.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 export var grid_size: Vector2 = Vector2(8, 8)
-var grid = []
-var piece_defs = {}
 export var PIECE_DEF_JSON : String
+
+var grid = []
 var list_pieces: Array = []
+var piece_defs : Dictionary = {}
 
 func _ready():
 	randomize()
@@ -22,15 +23,50 @@ func _ready():
 			list_pieces.append(k)
 			
 	print(list_pieces)
+	get_legal_moves("king", get_pos($Player))
+
+func _process(delta: float) -> void:
+	get_pos($Player)
 
 func load_JSON(file_path):
 	# example of file path: "res://Ress/panelTextn2.json"
 	# ref: https://godotengine.org/qa/8291/how-to-parse-a-json-file-i-wrote-myself
-	var dict = {}
+	var dict : Dictionary = {}
 	var file = File.new()
 	file.open(file_path, file.READ)
 	var text = file.get_as_text()
 	dict = parse_json(text)
 	file.close()
 	return dict
-			
+	
+func get_legal_moves(piece, current_grid_pos):
+	#return the cells where the piece can move
+	var piece_moves = []
+	var valid_moves = []
+	piece_moves = get_moves(piece)
+
+	for move in piece_moves:
+		if is_cell_vacant(current_grid_pos, move["step"]):
+			valid_moves.append(move["step"])
+	return valid_moves #placeholder
+
+func is_cell_vacant(current_grid_pos, move) -> bool:
+	#check if the cell where the piece wants to move is empty or not
+	var next_grid_pos = current_grid_pos + move
+	return true #placeholder
+
+
+func get_moves(piece):
+	#return an array of dictionaries containing the moves of the parameter piece
+	return piece_defs[piece]["moves"]
+
+func get_pos(piece):
+	#return the position's array of the piece
+	return [ceil(piece.position.x / 64), ceil(piece.position.x / 64)]
+
+
+
+
+
+
+
